a257c858835ba1912e4689052d482e1f288ea7a0
MetaRadiator <35647950+radimowave@users.noreply.github.com>
2024-08-21 11:57:43 +0700

    Create InABOut_words.ino

---
+++ InABOut_words.ino
@@ -0,0 +1,20 @@
+void setup() {
+  // Start serial communication at 9600 baud rate
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Check if there's incoming serial data
+  if (Serial.available() > 0) {
+    // Read the incoming character
+    char incomingByte = Serial.read();
+
+    // Check if it's 'A' or 'B' and print the appropriate message
+    if (incomingByte == 'A') {
+      Serial.println("Meta");
+    } 
+    else if (incomingByte == 'B') {
+      Serial.println("ESE@KKU");
+    }
+  }
+}
